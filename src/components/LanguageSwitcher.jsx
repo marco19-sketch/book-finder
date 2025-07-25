9c3d65b55aa083ea7238f34f07c46ad58fe70543
marco19-sketch <marco.brusca1970@gmail.com>
2025-07-25 09:46:48 +0200

    changed language selectors

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,8 +1,9 @@
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
+import gbFlag from "../assets/32X24/gb.png";
+import itFlag from "../assets/32x24/it.png";
 
 export default function LanguageSwitcher() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const changeLanguage = lng => {
     i18n.changeLanguage(lng);
@@ -10,8 +11,16 @@ export default function LanguageSwitcher() {
 
   return (
     <div>
-      <button className={`english ${i18n.language === 'en' ? 'selected' : ''}`} onClick={() => changeLanguage("en")}>Eng</button>
-      <button className={`italiano ${i18n.language === 'it' ? 'selected' : ''}`} onClick={() => changeLanguage("it")}>Ita</button>
+      <button
+        className={`english ${i18n.language === "en" ? "selected" : ""}`}
+        onClick={() => changeLanguage("en")}>
+        <img src={gbFlag} alt={t("englishFlag")} />
+      </button>
+      <button
+        className={`italiano ${i18n.language === "it" ? "selected" : ""}`}
+        onClick={() => changeLanguage("it")}>
+        <img src={itFlag} alt={t("italianFlag")} />
+      </button>
     </div>
   );
 }
