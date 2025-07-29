b2b784b2894999014871a7eab63f367bdbea2b2c
marco19-sketch <marco.brusca1970@gmail.com>
2025-07-29 21:39:29 +0200

    Optimized i18next for translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,19 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import en from './locales/en.json';
-import it from './locales/it.json';
-
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpBackend from "i18next-http-backend";
 
 i18n
-    .use(initReactI18next)
-    .init({
-        resources: {
-            en: { translation: en },
-            it: { translation: it }
-        },
-        lng: 'en', 
-        fallback: 'en',
-        interpolation: {
-            escapeValue: false,
-        },
-    });
+  .use(HttpBackend) // abilita fetch via HTTP
+  .use(initReactI18next)
+  .init({
+    lng: "en", // o 'it'
+    fallbackLng: "en",
+    backend: {
+      loadPath: "/locales/{{lng}}.json", // fetch da /public/locales/it.json, ecc
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
