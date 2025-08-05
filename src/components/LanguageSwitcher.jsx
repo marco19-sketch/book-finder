54fb6093fcfef5304ba6df7e0f0d6bd2de3365b9
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-05 19:20:04 +0200

    Using srcSet with LanguageSwitcher.jsx for flag icons

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
-import gbFlag from "../assets/images/32x24/gb.webp";
-import itFlag from "../assets/images/32x24/it.webp";
+// import gbFlag from "../assets/images/32x24/gb.webp";
+// import itFlag from "../assets/images/32x24/it.webp";
 
 export default function LanguageSwitcher() {
   const { t, i18n } = useTranslation();
@@ -14,12 +14,28 @@ export default function LanguageSwitcher() {
       <button
         className={`english ${i18n.language === "en" ? "selected" : ""}`}
         onClick={() => changeLanguage("en")}>
-        <img src={gbFlag} alt={t("englishFlag")} />
+        {/* <img src={gbFlag} alt={t("englishFlag")} /> */}
+        <img
+          loading="lazy"
+          width="32"
+          height="24"
+          src={`/flags/32x24/gb.webp`}
+          srcSet={`/flags/32x24/gb.webp 1x, /flags/64x48/gb.webp 2x`}
+          alt={t("englishFlag")}
+        />
       </button>
       <button
         className={`italiano ${i18n.language === "it" ? "selected" : ""}`}
         onClick={() => changeLanguage("it")}>
-        <img src={itFlag} alt={t("italianFlag")} />
+        {/* <img src={itFlag} alt={t("italianFlag")} /> */}
+        <img
+          loading="lazy"
+          width="32"
+          height="24"
+          src={`/flags/32x24/it.webp`}
+          srcSet={`/flags/32x24/it.webp 1x, /flags/64x48/it.webp 2x`}
+          alt={t("englishFlag")}
+        />
       </button>
     </div>
   );
