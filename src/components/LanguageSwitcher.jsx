974d594e319a7c48c063135e9a447b4b312a01ca
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-05 21:27:57 +0200

    Deleted FooterRefactoring.jsx, commented loading lazy to the flags in LanguageSwitcher.jsx

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -16,25 +16,26 @@ export default function LanguageSwitcher() {
         onClick={() => changeLanguage("en")}>
         {/* <img src={gbFlag} alt={t("englishFlag")} /> */}
         <img
-          loading="lazy"
+          // loading="lazy"
           width="32"
           height="24"
           src={`/flags/32x24/gb.webp`}
-          srcSet={`/flags/32x24/gb.webp 1x, /flags/64x48/gb.webp 2x`}
+          srcSet={`/flags/32x24/gb.webp 1x, /flags/48x36/gb.webp 2x`}
           alt={t("englishFlag")}
         />
       </button>
       <button
         className={`italiano ${i18n.language === "it" ? "selected" : ""}`}
         onClick={() => changeLanguage("it")}>
+
         {/* <img src={itFlag} alt={t("italianFlag")} /> */}
         <img
-          loading="lazy"
+          // loading="lazy"
           width="32"
           height="24"
           src={`/flags/32x24/it.webp`}
-          srcSet={`/flags/32x24/it.webp 1x, /flags/64x48/it.webp 2x`}
-          alt={t("englishFlag")}
+          srcSet={`/flags/32x24/it.webp 1x, /flags/48x36/it.webp 2x`}
+          alt={t("italianFlag")}
         />
       </button>
     </div>
